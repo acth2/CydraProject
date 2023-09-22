fdf7047651c3ea52242df1f71076d3bd51c17928
AcTh2 <68511655+acth2@users.noreply.github.com>
2023-09-22 18:58:07 +0200

    Update cache.sh

--- testing/packagemanagers/software/cache.sh
+++ testing/packagemanagers/software/cache.sh
@@ -1,10 +1,10 @@
 # Déclarer un tableau pour stocker les lignes
-declare -a depsList
+declare -a lines
 
 # Utiliser awk pour extraire les lignes et les stocker dans le tableau
-depsList=($(awk '/%DEPENDS%/ {flag=1; next} flag {print; if (/^$/) exit}' ${DEPENDS_FILE}))
+lines=($(awk '/%DEPENDS%/ {flag=1; next} flag && !/^$/ {print; if (/^$/) exit}' exemple.txt))
 
 # Afficher les lignes du tableau
 for line in "${lines[@]}"; do
-    echo "$depsList"
+    echo "$line"
 done

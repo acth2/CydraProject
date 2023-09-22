8a540a2c451bb0b50c7c8dad36f552b0f350a882
AcTh2 <68511655+acth2@users.noreply.github.com>
2023-09-22 19:04:17 +0200

    Update cache.sh

--- testing/packagemanagers/software/cache.sh
+++ testing/packagemanagers/software/cache.sh
@@ -2,7 +2,7 @@
 declare -a lines
 
 # Utiliser awk pour extraire les lignes et les stocker dans le tableau
-lines=($(awk '/%DEPENDS%/ {flag=1; next} flag && !/^$/ {print; if (/^$/) exit}' exemple.txt))
+lines=($(awk '/%DEPENDS%/ {flag=1; next} flag && !/^$/ {print; if (/^$/) {flag=0; exit}}' /etc/cydramanager/cache/toilet-0.3.r155.3eb9d58-2/desc))
 
 # Afficher les lignes du tableau
 for line in "${lines[@]}"; do

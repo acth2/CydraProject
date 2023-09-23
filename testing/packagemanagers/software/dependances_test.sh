ab23c9de3dcaaa53de6739606b9fd49799e3a7b0
AcTh2 <68511655+acth2@users.noreply.github.com>
2023-09-23 09:25:27 +0200

    Update dependances_test.sh

--- testing/packagemanagers/software/dependances_test.sh
+++ testing/packagemanagers/software/dependances_test.sh
@@ -19,3 +19,25 @@ for line in "${lines[@]}"; do
         cydramanager install $line --without-printing-log --without-registering-var --add-as-depends
     fi
 done
+
+declare -a lines2
+flag1 = 0
+
+while read -r line3; do
+    lines2+=("$line3")
+    if [[ "$line3" == *"%*"* ]]; then
+        break
+    fi
+    if [[ "$line3" == "%MAKEDEPENDS%" ]]; then
+        break
+        flag1 = 1
+    fi
+done < <(awk '/%OPTDEPENDS%/ {flag=1; next} flag && !/^$/ {print; if (/^$/) exit}' $DEPENDS_INFO)
+
+for line in "${lines2[@]}"; do
+    echo "$line3"
+    if [[ "$flag1" == "0" ]]; then
+        echo "$line3"
+        cydramanager install $line3 --without-printing-log --without-registering-var --add-as-depends
+    fi
+done

2b7a5371b298e0052f8ecc091f3018eb7624d2f7
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-05-07 17:39:16 +0200

    Update install.sh

--- testing/packagemanagers/install/install.sh
+++ testing/packagemanagers/install/install.sh
@@ -53,7 +53,8 @@ function install_files {
     touch /etc/cydraterms/usersoftware.list
     chmod +rwx /etc/cydraterms/usersoftware.list
     wget "https://raw.githubusercontent.com/acth2/CydraProject/main/packagemanager/changelogs.log" -P /etc/cydraterms --no-check-certificate -q
-    wget https://raw.githubusercontent.com/acth2/CydraProject/main/packagemanager/basicmirror.list -P /etc/cydraterms/mainserver.list --no-check-certificate -q
+    wget "https://raw.githubusercontent.com/acth2/CydraProject/main/packagemanager/basicmirror.list" -P /etc/cydrafetch/currentMirror.list --no-check-certificate -q
+    wget "https://raw.githubusercontent.com/acth2/CydraProject/main/packagemanager/fetch/mainserver.list" -P /etc/cydraterms/mainserver.list
 
     touch /etc/cydrafetch/1.mirror
     touch /etc/cydrafetch/2.mirror

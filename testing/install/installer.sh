955f28e7a27c3c3361a52a4b6abbdc4b4d74c152
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-08-10 00:27:39 +0200

    Update installer.sh

--- testing/install/installer.sh
+++ testing/install/installer.sh
@@ -297,7 +297,7 @@ function GRUB_CONF {
     echo "" >> "/mnt/efi/boot/grub/grub.cfg"
     echo 'menuentry "GNU/Linux, CydraLite Release V2.0"  {' >> "/mnt/efi/boot/grub/grub.cfg"
     echo "  echo Loading GNU/Linux CydraLite V02..." >> "/mnt/efi/boot/grub/grub.cfg"
-    echo "  linux /boot/vmlinuz-5.19.2 root=UUID=${chosen_partition}1 ro quiet" >> "/mnt/efi/boot/grub/grub.cfg"
+    echo "  linux /boot/vmlinuz-5.19.2 root=${chosen_partition}1 ro quiet" >> "/mnt/efi/boot/grub/grub.cfg"
     echo "  echo Loading ramdisk..." >> "/mnt/efi/boot/grub/grub.cfg"
     echo "  initrd /boot/initrd.img-5.19.2" >> "/mnt/efi/boot/grub/grub.cfg"
     echo "}" >> "/mnt/efi/boot/grub/grub.cfg"

8a23402f8839dae0f59ce2b7210afbcaf76595f2
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-15 20:07:29 +0200

    Update qterminal-quick.sh

--- testing/qterminal-quick.sh
+++ testing/qterminal-quick.sh
@@ -8,7 +8,7 @@ wget https://github.com/lxqt/qterminal/releases/download/1.3.0/qterminal-1.3.0.t
 # cd       build &&
 
 # cmake -DCMAKE_INSTALL_PREFIX=/usr \
-#      -DCMAKE_BUILD_TYPE=Release  \
+#      -DCMAKE_BUILD_TYPE=Release  \amongys
 #      .. &&
 
 # make
